6e63d7ce18586a44888220e4e86d1cc33dd2ec4d
Nigel Meyer <nigelmeyer42@gmail.com>
2021-05-18 15:23:04 -0400

    added .js and .css pages
    added and linked javascript and css pages to project

---
+++ apps.js
@@ -0,0 +1,33 @@
+
+window.addEventListener("DOMContentLoaded" , function(e) {
+    console.log("Page Loaded")
+});
+
+const addForm = document.forms['name'];
+
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+    replaceName();
+        function replaceName() {
+            var name = document.getElementsByClassName("fName");
+            for (var i = 0; i < name.length; i++) {
+                name[i].innerHTML = value;
+            }
+        }
+    displayText();
+        function displayText() {
+            var texts = document.getElementsByClassName("hidden");
+            for (var i = 0; i < texts.length; i++) {
+                texts[i].style.display = "block";
+            }
+        }
+    hideIntro();
+    function hideIntro() {
+            var intro = document.getElementsByClassName("intro");
+            for (var i = 0; i < intro.length; i++) {
+                intro[i].style.display = "none";
+            }
+        }
+});
