446dd0c3a7e07386ae6961479bb95f7cf371d533
Nigel Meyer <nigelmeyer42@gmail.com>
2021-05-24 19:44:54 -0400

    catch up commit

--- apps.js
+++ apps.js
@@ -36,18 +36,3 @@ addForm.addEventListener('submit', function(e) {
      }
 
 });
-
-function insertValue() {
-    if (value === "") {
-        document.getElementsByClassName("errorMessage")[0].style.display = "block";
-    } else {
-        console.log(value);
-        replaceName();
-            function replaceName() {
-                var name = document.getElementsByClassName("fName");
-                for (var i = 0; i < name.length; i++) {
-                    name[i].innerHTML = value;
-                }
-            }
-        }
-};
