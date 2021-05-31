8818b60cc4506c67fd7cfcbd34879d741a1f6f7d
Nigel Meyer <nigelmeyer42@gmail.com>
2021-05-31 10:28:36 -0500

    moved addForm event listener into if statement

--- apps.js
+++ apps.js
@@ -1,54 +1,60 @@
 
 window.addEventListener("DOMContentLoaded" , function(e) {
     console.log("Page Loaded")
-    
-});
 
+    const addForm = document.forms['name'];
 
+    if (addForm) {
+        addForm.addEventListener('submit', function(e) {
+            e.preventDefault();
+            const value = addForm.querySelector('input[type="text"]').value;
+            if (value === "") {
+                document.getElementsByClassName("errorMessage")[0].style.display = "block"
+            } else {
+                console.log(value);
 
-const addForm = document.forms['name'];
+                const saveToLocalStorage = () => {
+                    localStorage.setItem('nameInput', value)
+                }
 
+                saveToLocalStorage();
 
-addForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const value = addForm.querySelector('input[type="text"]').value;
-    if (value === "") {
-        document.getElementsByClassName("errorMessage")[0].style.display = "block"
-    } else {
-        console.log(value);
+                const storedInput = localStorage.getItem('nameInput');
 
-        const saveToLocalStorage = () => {
-            localStorage.setItem('nameInput', value)
+                replaceName();
+                    function replaceName() {
+                        let name = document.getElementsByClassName("spanName");
+                        for (var i = 0; i < name.length; i++) {
+                            name[i].innerHTML = storedInput;
+                        }
+                    }
+                displayText();
+                    function displayText() {
+                        var texts = document.getElementsByClassName("hidden");
+                        for (var i = 0; i < texts.length; i++) {
+                            texts[i].style.display = "block";
+                        }
+                    }
+
+                hideIntro();
+                function hideIntro() {
+                        var intro = document.querySelectorAll(".intro,.errorMessage");
+                        for (var i = 0; i < intro.length; i++) {
+                            intro[i].style.display = "none";
+                        }
+                    }
+                }
+
+            });
         }
+    }); 
+    const storedInput = localStorage.getItem('nameInput');
 
-        saveToLocalStorage();
-
-        const storedInput = localStorage.getItem('nameInput');
-
-        replaceName();
-            function replaceName() {
-                let name = document.getElementsByClassName("spanName");
-                for (var i = 0; i < name.length; i++) {
-                    name[i].innerHTML = storedInput;
-                }
-            }
-        displayText();
-            function displayText() {
-                var texts = document.getElementsByClassName("hidden");
-                for (var i = 0; i < texts.length; i++) {
-                    texts[i].style.display = "block";
-                }
-            }
-
-        hideIntro();
-        function hideIntro() {
-                var intro = document.querySelectorAll(".intro,.errorMessage");
-                for (var i = 0; i < intro.length; i++) {
-                    intro[i].style.display = "none";
-                }
-            }
-     }
-
-});
-
-document.getElementsByClassName("spanName").innerHTML = localStorage.getItem("nameInput");
+    replaceName();
+                    function replaceName() {
+                        let name = document.getElementsByClassName("spanName");
+                        for (var i = 0; i < name.length; i++) {
+                            name[i].innerHTML = storedInput;
+                        }
+                    }
+// document.getElementsByClassName("spanName").item(0).innerHTML = localStorage.getItem("nameInput");
